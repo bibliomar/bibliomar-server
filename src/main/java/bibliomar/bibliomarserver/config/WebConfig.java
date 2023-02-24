9f04f964b62e9c0225fb0d474cb36d92e1a4dce5
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2023-02-23 21:45:56 -0300

    Add new Vite v4 server to CORS

--- src/main/java/bibliomar/bibliomarserver/config/WebConfig.java
+++ src/main/java/bibliomar/bibliomarserver/config/WebConfig.java
@@ -15,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000",
                         "http://localhost:3001",
+                        "http://localhost:5173",
                         "https://bibliomar.netlify.app",
                         "https://bibliomar.site",
                         "https://www.bibliomar.site",
